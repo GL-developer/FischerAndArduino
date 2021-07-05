cc94c2f396e181a4f0b4b30abeae18f1503d1aae
GL-developer <glcompanydeveloper@gmail.com>
2021-07-05 10:52:28 +0900

    add turn table step2, 3

---
+++ turn_table/step2/step2.ino
@@ -0,0 +1,23 @@
+int input1 = 10;
+int input2 = 13;
+
+void setup() {
+  pinMode(input1, OUTPUT);
+  pinMode(input2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+   digitalWrite(input1, HIGH) ;
+   digitalWrite(input2, LOW) ;
+   delay(5000);
+   digitalWrite(input1, LOW) ;
+   digitalWrite(input2, LOW) ;
+   delay(3000);
+   digitalWrite(input1, LOW);
+   digitalWrite(input2, HIGH);
+   delay(5000) ;
+   digitalWrite(input1, LOW) ;
+   digitalWrite(input2, LOW) ;
+   delay (3000) ;
+}
